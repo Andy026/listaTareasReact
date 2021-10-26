4e8d593af68889ba9b83e5a2fcb3ad8e01574dee
Andy <andysatie@gmail.com>
2021-10-26 11:28:18 -0300

    se creó la estructura básica de un proyecto de react, agregamos 2 componentes e instalamos bootstrap

---
+++ src/components/Subtitulo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//se autocompleta con rsc (react stateless component)
+const Subtitulo = () => {
+        //acá va la lógica
+    return (
+        //acá va el maquetado y un poco de lógica
+        <h2>
+            Tareas de la comisión
+        </h2>
+    );
+};
+
+export default Subtitulo;
---
+++ src/components/Titulo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+//se autocompleta con rcc (react class component)
+class Titulo extends Component {
+    render() {
+        return (
+            <h1>Lista de tareas</h1>
+        );
+    }
+}
+
+export default Titulo;
