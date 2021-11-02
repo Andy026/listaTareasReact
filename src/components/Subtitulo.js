c8fbf609e285f055ec998c5c68e788e0cce657bb
Andy <andysatie@gmail.com>
2021-11-02 10:06:38 -0300

    agregamos estetica a los componentes Titulo y Subtitulo y ademas agregamos 2 props

--- src/components/Subtitulo.js
+++ src/components/Subtitulo.js
@@ -1,11 +1,9 @@
 import React from 'react';
 //se autocompleta con rsc (react stateless component)
-const Subtitulo = () => {
-        //acá va la lógica
+const Subtitulo = (props) => {
     return (
-        //acá va el maquetado y un poco de lógica
-        <h2>
-            Tareas de la comisión
+        <h2 className="text-center">
+            Tareas de la comisión {props.comision}
         </h2>
     );
 };
